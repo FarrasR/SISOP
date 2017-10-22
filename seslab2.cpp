373eb0bd2008d387b60d2172fcc813aec8a651f0
FarrasR <farrasrahmatullah@gmail.com>
2017-10-22 19:43:20 +0700

    nomor 2 dah selesai

--- seslab2.cpp
+++ seslab2.cpp
@@ -5,14 +5,17 @@
 #include<unistd.h>
 #include<math.h>
 #include<vector>
-
+using namespace std;
 int status;
-vector <int> primes;
+int awal;
+int akhir;
+
+vector<int> primes;
 
 bool is_prime(int i)
 {
 	int u=(int)sqrt(i);
-	for (int j=0;primes[j]<=u&&j<primes.size();++j) {
+	for (int j=0; primes[j]<=u&&j<primes.size();++j) {
 		if (i%primes[j]==0)
 		return false;
 	}
@@ -21,17 +24,16 @@ bool is_prime(int i)
 
 
 
-void* searchprime(void *arg, int awal, int akhir){
-
-
+void* searchprime(void *arg){
     while(status != 1)
     status=1;
     for(int i=awal; i<=akhir; i++)
     {
+	//printf("COK %d\n",i);
        	if (is_prime(i))
 	{
-	primes.push_back(i);
-	    printf("%d\n",i);	
+	    primes.push_back(i);
+	    printf("%d\n",i);
 	}
     }
     status=0;
@@ -40,30 +42,28 @@ void* searchprime(void *arg, int awal, int akhir){
 
 int main()
 {
-    prima.push_back(2);
-    int i=0;
-    int err;    
+    primes.push_back(2);
+    int err;
     int n, t;
     scanf("%d%d",&n,&t);
     pthread_t tid[t];
-    int awal=2;
-    int bagi=n/t
-    status=0;
-    while(i<t)
+    awal=2;
+    akhir=n/t;
+	status=0;
+    for(int i=0; i<t; i++)
     {
-        err = pthread_create(&(tid[i]),NULL,&searchprime(awal,bagi),NULL);
+        err = pthread_create(&(tid[i]),NULL,&searchprime,NULL);
         if(err!=0) printf("\ncan't create thread: [%s]",strerror(err));
-    	else printf("\nthread made");
-	    sleep(1);
-	    i++;
-        awal=bagi+1;
-        bagi+=bagi;
-        if(awal+bagi-1>n)bagi=n; 
-    }     
+    	else printf("\nthread made %d\n",i);
+	sleep(1);
+        awal=akhir+1;
+        akhir+=akhir;
+        if(awal+akhir-1>n)akhir=n;
+    }
     for(int j=0; j<t; j++)
     {
         pthread_join(tid[j],NULL);
-    }         
+    }
 }
 
 
