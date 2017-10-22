c8465d00044709428ec03590f79024cb81d77dc9
FarrasR <farrasrahmatullah@gmail.com>
2017-10-22 15:42:01 +0700

    ganti ke windows, vbox lambat

--- seslab2.c
+++ seslab2.c
@@ -6,9 +6,26 @@
 #include<math.h>
 
 
-void* searchprime(void *arg){
+int status;
+int prima[150];
+int primesize;
+
+void* searchprime(void *arg, int awal, int akhir){
+
+
+    while(status != 1)
+    {
+    }
+    status=1;
+    for(int i=awal; i<=akhir; i++)
+    {
+        if(
+    }
+    status=0;
+
         
-    
+
+    return NULL
 }
 
 int main()
@@ -17,13 +34,20 @@ int main()
     int err;    
     int n, t;
     scanf("%d%d",&n,&t);
+    pthread_t tid[t];
+    int awal=2;
+    int bagi=n/t
+    status=0;
     while(i<t)
     {
-        err = pthread_create(&(tid[i]),NULL,&copyandcopy,NULL);
+        err = pthread_create(&(tid[i]),NULL,&searchprime(awal,bagi),NULL);
         if(err!=0) printf("\ncan't create thread: [%s]",strerror(err));
     	else printf("\nthread made");
 	    sleep(1);
-	    i++; 
+	    i++;
+        awal=bagi+1;
+        bagi+=bagi;
+        if(i==t-1)bagi=n; 
     }     
     for(int j=0; j<t; j++)
     {
